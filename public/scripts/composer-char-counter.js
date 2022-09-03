8a7d6906e21c46f0bfb82326cad464e63a490ba8
atyoshimatsu <at.yoshimatsu@gmail.com>
2022-09-02 23:42:28 -0700

    Modify composer-char-counter to set font color in case of over 0 length

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
   $('#tweet-text').on('input', function(e) {
     const counter = 140 - $(this).val().length;
+    const counterColor = counter >= 0 ? '' : 'red';
     const counterDOM = $(this).next().children('.counter');
+
     counterDOM.val(counter);
-    if (counter < 0) {
-      counterDOM.css('color', 'red');
-    }
+    counterDOM.css('color', counterColor);
   });
 });
