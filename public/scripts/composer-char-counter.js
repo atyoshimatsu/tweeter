fa715d460984e36f0226e803eeceb9ed2c571ccd
atyoshimatsu <at.yoshimatsu@gmail.com>
2022-09-03 01:28:16 -0700

    Solve an eslint error

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  $('#tweet-text').on('input', function(e) {
+  $('#tweet-text').on('input', function() {
     const counter = 140 - $(this).val().length;
     const counterColor = counter >= 0 ? '' : 'red';
     const counterDOM = $(this).next().children('.counter');
