3650e682b21a4db59b4120446bb9e8a4399ed9f6
atyoshimatsu <at.yoshimatsu@gmail.com>
2022-09-09 18:22:02 -0700

    Add scroll back function

--- public/scripts/composer.js
+++ public/scripts/composer.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+const charCounter = () => {
   $('#tweet-text').on('input', function() {
     const counter = 140 - $(this).val().replace(/\r?\n/g, '').length; // not count \r\n or n
     const counterColor = counter >= 0 ? '' : 'red';
@@ -7,4 +7,25 @@ $(document).ready(function() {
     counterDOM.val(counter);
     counterDOM.css('color', counterColor);
   });
+}
+
+const scrollBack = () => {
+  $(window).on('scroll', function() {
+    if ($(this).scrollTop() > 120) {
+      $('#scroll-button').show();
+      $('.nav-new-tweet').hide();
+    }
+  });
+
+  $('#scroll-button').on('click', function() {
+    $('html, body').animate({ scrollTop: 0 }, 300, 'swing', function() {
+      $('.nav-new-tweet').show();
+      $('#scroll-button').hide();
+    });
+  });
+};
+
+$(document).ready(function() {
+  charCounter();
+  scrollBack();
 });
