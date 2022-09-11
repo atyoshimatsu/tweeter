574d11a91dacd3a8c990362e7e9629137913af88
atyoshimatsu <at.yoshimatsu@gmail.com>
2022-09-11 11:25:30 -0700

    Modify the comment

--- public/scripts/composer.js
+++ public/scripts/composer.js
@@ -1,6 +1,6 @@
 const charCounter = () => {
   $('#tweet-text').on('input', function() {
-    const counter = 140 - $(this).val().replace(/\r?\n/g, '').length; // not count \r\n or n
+    const counter = 140 - $(this).val().replace(/\r?\n/g, '').length; // not count \r\n or \n
     const counterColor = counter >= 0 ? '' : 'red';
     const counterDOM = $(this).next().children('.counter');
 
